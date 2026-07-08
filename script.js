0392b9990d5d265c1efca87b26f78f494f437d82
jashwanthreddy2508-commits <jashwanthreddy2508@gmail.com>
2026-07-08 22:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function toupper(str) {
 	return str.toUpperCase()
 }
+const input= document.getElementById("fname");
+input.addEventListener("blur", function () {
+	input.value= toupper(this.value)
+	
+})
